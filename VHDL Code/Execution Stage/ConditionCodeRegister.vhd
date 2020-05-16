3a6b9c110c0336129405dfd8ef72e7bb1101b465
Mahmood Mohamed <mmmacmp@gmail.com>
2020-05-16 17:08:34 +0200

    Add writeEnable to condition code register

--- VHDL Code/Execution Stage/ConditionCodeRegister.vhd
+++ VHDL Code/Execution Stage/ConditionCodeRegister.vhd
@@ -2,7 +2,7 @@ library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
 ENTITY ConditionCodeRegister IS
- PORT ( clock, reset : IN STD_LOGIC;
+ PORT ( clock, reset,writeEnable : IN STD_LOGIC;
  CCRIN : IN STD_LOGIC_VECTOR (3 downto 0);
  CCROUT : OUT STD_LOGIC_VECTOR (3 downto 0) );
 END ConditionCodeRegister; 
@@ -13,7 +13,7 @@ ARCHITECTURE behaviour OF ConditionCodeRegister IS
  BEGIN
 IF (reset = '1') THEN
 CCROUT <= (OTHERS=>'0');
-ELSIF (rising_edge(clock)) THEN
+ELSIF (rising_edge(clock)and writeEnable='1') THEN
 CCROUT <= CCRIN;
 END IF;
 END PROCESS;
