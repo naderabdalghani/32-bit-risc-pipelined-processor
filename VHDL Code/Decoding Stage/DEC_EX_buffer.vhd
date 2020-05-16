0acde2e3ef7903265963231dd42e0fd78499d050
sarahRaafat15 <sararaafat1@yahoo.com>
2020-05-16 13:41:43 +0200

    Update DEC_EX_buffer.vhd

--- VHDL Code/Decoding Stage/DEC_EX_buffer.vhd
+++ VHDL Code/Decoding Stage/DEC_EX_buffer.vhd
@@ -7,16 +7,19 @@ Readdata1:in std_logic_vector(31 downto 0);
 Readdata:in  std_logic_vector(31 downto 0);
 EA: in  std_logic_vector(19 downto 0);
 IMM:in std_logic_vector(15 downto 0);
+TWO_FETCHES_FROM_FETCHING:in std_logic ;
 TWO_FETCHES:in std_logic_vector(1 downto 0);
 PREDICTION_SIGNAL:in std_logic;
 PC:in  std_logic_vector(31 downto 0);
 WRITE_REG1:in std_logic_vector(2 downto 0);
 WRITE_REG2:in std_logic_vector(2 downto 0);
-clk:in std_logic
+clk:in std_logic;
+Rst:in std_logic
 );
 end DEC_EX_buffer;
 architecture Behavioral of DEC_EX_buffer is
 signal Readdata2:std_logic_vector(31 downto 0);
+
 begin
 
 Readdata2 <= Readdata when TWO_FETCHES ="00"
@@ -24,9 +27,12 @@ else "000000000000" &EA when TWO_FETCHES ="01"
 else std_logic_vector(resize(signed(IMM), Readdata2'length))when TWO_FETCHES ="10"
 else Readdata; 
 
- process(clk) 
+ process(clk,TWO_FETCHES_FROM_FETCHING,Rst) 
  begin
-if(rising_edge(clk)) then
+if(TWO_FETCHES_FROM_FETCHING= '1' or Rst='1') then
+buffer_output <= (others => '0');
+
+elsif(rising_edge(clk)) then
 
 buffer_output <= PREDICTION_SIGNAL & Readdata1 & Readdata2 & PC & WRITE_REG1 & WRITE_REG2;
 
