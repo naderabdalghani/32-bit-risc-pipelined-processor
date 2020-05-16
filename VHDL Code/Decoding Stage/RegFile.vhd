80235063b7d2132bcd7beb4999ed19d0e07b52b1
sarahRaafat15 <sararaafat1@yahoo.com>
2020-05-16 20:30:23 +0200

    Update RegFile.vhd

--- VHDL Code/Decoding Stage/RegFile.vhd
+++ VHDL Code/Decoding Stage/RegFile.vhd
@@ -12,7 +12,8 @@ Rdst,Rsrc1,Rsrc2:in std_logic_vector(2 downto 0);
 WB1,WB2:in std_logic;
 Clk,rst:in std_logic ;
 address_3:in std_logic_vector(2 downto 0);
-data_3:out std_logic_vector(2 downto 0)
+data_3:out std_logic_vector(31 downto 0);
+REG0,REG1,REG2,REG3,REG4,REG5,REG6,REG7:out std_logic_vector(31 downto 0)
 );
 end RegFile ;
 architecture Behavioral of RegFile is
@@ -67,5 +68,13 @@ Readdata2 <= Registers(to_integer(unsigned(READ_ADDRESS2)));
 Read_address_1<=READ_ADDRESS1 ;
 Read_address_2<=READ_ADDRESS2 ;
 
-data_3 <= address_3;
+data_3 <= Registers(to_integer(unsigned(address_3)));
+REG0 <= Registers(0);
+REG1 <= Registers(1);
+REG2 <= Registers(2);
+REG3 <= Registers(3);
+REG4 <= Registers(4);
+REG5 <= Registers(5);
+REG6 <= Registers(6);
+REG7 <= Registers(7);
 end Behavioral ;
